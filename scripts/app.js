fd7836b0dc719bb71d927980c9b8ce495508c60a
aml131 <hopealasfoor@gmail.com>
2025-07-14 23:16:37 +0300

    Java Script

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
-const paperImg=[
- "../assets/exam1.png",
- "../assets/exam3.png",
- "../assets/exam4.png",
- "../assets/exam5.png",
- "../assets/exam6.png"
+const paperImg = [
+  "../assets/exam1.png",
+  "../assets/exam3.png",
+  "../assets/exam4.png",
+  "../assets/exam5.png",  "../assets/exam6.png"
 ]
-/*---------------------------- Variables (state) ----------------------------*/
+const sliceSound = document.getElementById("sliceAudio")
+const gameBoard = document.getElementsByClassName("gameBoard")[0]
+const scoreDisplay = document.getElementById("score");
+const missesDisplay = document.getElementById("misses");
 
+let score = 0
+let misses = 0let gameOver = false
 /*-------------------------------- Functions --------------------------------*/
-
-/*----------------------------- Event Listeners -----------------------------*/
