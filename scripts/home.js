bbbebbc5fcf04dd13b085150192eb8220b6cb227
aml131 <hopealasfoor@gmail.com>
2025-07-17 09:55:07 +0300

    modal

--- scripts/home.js
+++ scripts/home.js
@@ -2,3 +2,14 @@ const playBtn = document.getElementById('playBtn')
 playBtn.addEventListener("click",()=>{
   this.window.location.href='../index.html'
 })
+//the modal
+const openBtn = document.getElementById('How2')
+const closeBtn = document.getElementById('close')
+const modal = document.getElementById('modal')
+
+openBtn.addEventListener('click',()=>{
+  modal.classList.add('open')
+})
+closeBtn.addEventListener('click',()=>{
+  modal.classList.remove('open')
+})
